c9b610675e51b415c40a91c2f4beb7f0d94f153d
MadhumithaAravind <madhulogu1996@gmail.com>
2024-12-29 16:17:50 +0530

    Added 3 methods in ClassA

--- src/test/java/com/test/ClassA.java
+++ src/test/java/com/test/ClassA.java
@@ -13,5 +13,17 @@ public class ClassA {
 	private void method3() {
 		System.out.println("3");
 	}
+	private void europe() {
+		System.out.println("europe");
+
+	}
+	private void australia() {
+		System.out.println("australia");
+
+	}
+	private void england() {
+		System.out.println("england");
+
+	}
 
 }
